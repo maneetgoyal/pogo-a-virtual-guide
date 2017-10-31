1e7ba15320b278c6b6c27eed13f050b97d21d118
maneetgoyal <maneetgoyal14@gmail.com>
2017-10-30 22:14:47 -0400

    Text Preprocessor Development Started

---
+++ LDAPreProcessor/parastripping.py
@@ -0,0 +1,30 @@
+# Importing Libraries
+import pandas as pd
+from bs4 import BeautifulSoup
+
+# Extracts text out <p>...</p>
+def parastripper(inputstring):
+    soup = BeautifulSoup(inputstring, "lxml")
+    outputstring = soup.get_text()
+    return outputstring
+
+# Combines title and body text and saves at outputpath
+def parastripping(csvpath, outputpath):
+    # Open Data File
+    try:
+        data = pd.read_csv(csvpath)
+        # Spliiting tags' string
+        for ind, ele in enumerate(data['title']):
+            data.set_value(ind, 'title', ele)
+            data.set_value(ind, 'body', parastripper(data['body'][ind]))  # Updating data frame with array of tags
+        # Open Output File
+        try:
+            df = pd.DataFrame({'id': data['id'], 'title': data['title'], 'body': data['body']})
+            df.to_csv(outputpath)
+        except PermissionError:
+            print("Permission to open the file denied")
+    except PermissionError:
+        print("Permission to open the file denied")
+
+
+parastripping("questions0.csv", "out0.csv")
---
+++ LDAPreProcessor/stopwords.py
@@ -0,0 +1,26 @@
+import spacy
+import re
+
+def getstopwordlist():
+    # Spacy's Stop word list
+    from spacy.en.language_data import STOP_WORDS
+    count_Spacy = len(STOP_WORDS)
+
+    # Stop words from Link Assistant
+    # https://www.link-assistant.com/seo-stop-words.html Oct 29 2017
+    try:
+        listo = open("stopwords.txt").read()
+        listo = re.split("\s+", listo)  # '/s' Matches Unicode whitespace characters; '+' for global search
+    except IOError:
+        print("Text File couldn't be opened")
+
+    link_assistant_set = set()
+    link_assistant_set.update(listo)
+
+    # Desired List
+    union_set = link_assistant_set.union(STOP_WORDS)  # 'set' of 683 words # include empty '' also
+    try:
+        union_set.remove('')
+    except KeyError:
+        print("'' wasn't there in the union set.")
+    return union_set  # 'set' of 683 words
