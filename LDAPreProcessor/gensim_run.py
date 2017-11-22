5764bb3ee53ebec951b7f4685f852b1b7461a873
Kumar <HarshVedant.Kumar@stantec.com>
2017-11-21 22:45:48 -0500

    updated gensim_run for passes and indentation

--- LDAPreProcessor/gensim_run.py
+++ LDAPreProcessor/gensim_run.py
@@ -13,16 +13,16 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 # import pyLDAvis.gensim
 
 if __name__ == '__main__':
-    for i in range (0,4): #change range to your range of files
-    dictionary = Dictionary.load("./tag_processed/dict" + str(i) + ".dict") #change file structure according to your folders
-    corpus = corpora.MmCorpus("./tag_processed/corpus" + str(i) + ".mm")
+    for i in range (0,5): #change range to your range of files
+        dictionary = Dictionary.load("./bucket_processed/dict" + str(i) + ".dict") #change file structure according to your folders
+        corpus = corpora.MmCorpus("./bucket_processed/corpus" + str(i) + ".mm")
 
 
-    ldamodel = LdaMulticore(corpus=corpus, num_topics=20, id2word=dictionary, workers = 3, passes = 4) #if you don't have 4 cores, change LdaMulticore to LdaModel and remove the "workers = 3" parameter, 
-    # you can run LdaMulticore on 2 cores however it's not advised
+        ldamodel = LdaMulticore(corpus=corpus, num_topics=20, id2word=dictionary, workers = 3, passes = 10) #if you don't have 4 cores, change LdaMulticore to LdaModel and remove the "workers = 3" parameter, 
+        # you can run LdaMulticore on 2 cores however it's not advised
 
-    ldamodel.show_topics()
+        ldamodel.show_topics()
 
-    ldamodel.save("./multicore_4pass/bucket"+ str(i) + "LDA") # change file structure according to your folders
+        ldamodel.save("./multicore_4pass/bucket"+ str(i) + "LDA") # change file structure according to your folders
 
-    # pyLDAvis.save_html(pyLDAvis.gensim.prepare(ldamodel, corpus, dictionary), "demo2.html")
+        # pyLDAvis.save_html(pyLDAvis.gensim.prepare(ldamodel, corpus, dictionary), "demo2.html")
